927a08b9e74cf4998035b202a43577e5a1d8135b
ChingLong/Dennis <90884939+122ChingLong@users.noreply.github.com>
2021-09-23 09:51:09 +0800

    Changed

--- new.py
+++ new.py
@@ -1,2 +1,11 @@
 
 Hello, this is LT
+start=input("Enter the function to be executed")
+if start =="1":
+    #Function 1
+elif start =="2":
+    #Function 2
+elif start=="3":
+    #Function 3
+elif start =="4":
+    #Function 4
