1da42ed9f9e6556863693c41429cd708c4c07e05
nsdkjh <21107424d@connect.polyu.hk>
2021-10-07 15:42:46 +0800

    finish the second function

--- new.py
+++ new.py
@@ -1,12 +1,16 @@
 
-Hello, this is LT
+def function2():
+    a = int(input("First number:"))
+    b = int(input("Second number:"))
+    c = int(input("Third number pls:"))
+    d=a+b+c
+    print("The sum of", a,",", b, "and", c, "is", d)
 
-Hello this ZC
 start=input("Enter the function to be executed")
 if start =="1":
     #Function 1
 elif start =="2":
-    #Function 2
+    function2()
 elif start=="3":
     #Function 3
 elif start =="4":
