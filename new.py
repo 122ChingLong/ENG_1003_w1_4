8cc65744d8c44b294234753aee0f0705cab8934c
Niel02020 <90884723+Niel02020@users.noreply.github.com>
2021-10-07 23:36:44 +0800

    Update new.py

--- new.py
+++ new.py
@@ -5,12 +5,21 @@ def function2():
     c = int(input("Third number pls:"))
     d=a+b+c
     print("The sum of", a,",", b, "and", c, "is", d)
-
+def function4():
+    a = int(input("number 1"))
+    b = int(input("number 2"))
+    if a == b :
+        print("a is equal to b")
+    elif a > b : 
+        print("a is larger than b")
+    elif a < b :
+        print("a is smaller than b")
+    
 start=input("Enter the function to be executed")
 if start =="1":
     #Function 1
 elif start =="2":
-    function2()
+    #function2()
 elif start=="3":
     #Function 3
 elif start =="4":
