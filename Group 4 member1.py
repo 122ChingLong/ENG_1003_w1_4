e7e78bb7198677dfa6fb218ca16a78842fe77289
Li Zhehang <90884889+LiZhehang@users.noreply.github.com>
2021-10-15 10:54:49 +0800

    Add files via upload

---
+++ Group 4 member1.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Oct  5 10:10:56 2021
+
+@author: ALIENWARE-13
+"""
+
+
+list=['Li Zhehang','Yang Zixi','Ching Long','Daniel Chan']
+print(list[0])
+print(list[1])
+print(list[2])
+print(list[3])
